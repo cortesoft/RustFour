2183fdf2cf17182f9116f064f1bdfc21aac8d759
Daniel Lockhart <cortesoft@gmail.com>
2017-10-12 22:25:38 -0700

    Determine winner, random game currently

--- connect_four_engine/src/game/board.rs
+++ connect_four_engine/src/game/board.rs
@@ -1,11 +1,13 @@
 
-pub fn new_board(num_rows: u8, num_columns: u8) -> Board {
+pub fn new_board(num_rows: u8, num_columns: u8, connect_number: u8) -> Board {
     let mut board = Board {
         num_rows,
         num_columns,
+        connect_number,
         last_move: num_rows + 1,
         columns: Vec::new(),
-        next_move: GamePiece::X
+        next_move: GamePiece::X,
+        num_moves: 0
     };
     while board.columns.len() < (num_columns as usize) {
         board.columns.push(Column::new(num_rows));
@@ -13,7 +15,8 @@ pub fn new_board(num_rows: u8, num_columns: u8) -> Board {
     board
 }
 
-enum GamePiece {
+#[derive(PartialEq)]
+pub enum GamePiece {
     X,
     O,
     Dash
@@ -45,17 +48,24 @@ impl Column {
         self.rows[(self.next_row as usize)] = piece;
         self.next_row += 1;
     }
+
+    fn have_room(&self) -> bool {
+        self.next_row < self.num_rows
+    }
 }
 pub struct Board {
     num_rows: u8,
     num_columns: u8,
     last_move: u8,
     columns: Vec<Column>,
-    next_move: GamePiece
+    next_move: GamePiece,
+    num_moves: u16,
+    connect_number: u8
 }
 
 impl Board {
     pub fn print(&self) {
+        println!("\n\n");
         for i in 0..self.num_columns {
             print!(" {} ", i);
         }
@@ -76,24 +86,102 @@ impl Board {
         }
     }
 
-    pub fn play_piece(&mut self, col: u8) {
-        if col >= self.num_columns {
-            panic!("Move outside of range");
-        }
-        let nm = self.get_next_move();
-        self.columns[(col as usize)].play_piece(nm);
-        match self.next_move {
-            GamePiece::X => self.next_move = GamePiece::O,
-            GamePiece::O => self.next_move = GamePiece::X,
-            GamePiece::Dash => panic!("We should never have dash!")
+    // 0 for no winner, 1 for player 1, 2 for player 2
+    pub fn have_winner(&self) -> u8 {
+        //We will go through each direction:
+        //up left, left, down left, down, up right, right, and down right
+        if self.streak(-1,1,0) || self.streak(-1,0,0) || self.streak(-1,-1,0) ||
+            self.streak(0,-1,0) || self.streak(1,1,0) || self.streak(1,0,0) ||
+            self.streak(1,-1,0) {
+            if let GamePiece::X = self.get_last_move() {
+                1
+            } else {
+                2
+            }
+        } else {
+            0
         }
     }
 
-    fn get_next_move(&self) -> GamePiece {
+    pub fn streak(&self, x_delta: i8, y_delta: i8, mut length: u8) -> bool {
+        if length == 0 {
+            length = self.connect_number;
+        }
+        let mut current_loc = self.last_move_location();
+        if (x_delta < 0 && current_loc.0 < length - 1) || (y_delta < 0 && current_loc.1 < length - 1){
+            return false;
+        }
+        let mut streak_length = 1; //current piece counts as one!
+        let piece = self.get_last_move();
+        while streak_length < length {
+            match x_delta {
+                -1 => current_loc.0 -= 1,
+                1 => current_loc.0 += 1,
+                _ => ()
+            }
+            match y_delta {
+                -1 => current_loc.1 -= 1,
+                1 => current_loc.1 += 1,
+                _ => ()
+            }
+            if self.game_piece_at(current_loc.0, current_loc.1) == piece {
+                streak_length += 1;
+            } else {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn play_piece(&mut self, col: u8) -> bool {
+        if self.valid_move(col) {
+            let nm = self.get_current_move();
+            self.columns[(col as usize)].play_piece(nm);
+            self.next_move = self.get_next_move();
+            self.last_move = col;
+            self.num_moves += 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn get_current_move(&self) -> GamePiece {
         if let GamePiece::X = self.next_move {
             GamePiece::X
         } else {
             GamePiece::O
         }
-    } 
+    }
+
+    fn get_next_move(&self) -> GamePiece {
+        if let GamePiece::X = self.next_move {
+            GamePiece::O
+        } else {
+            GamePiece::X
+        }
+    }
+
+    fn get_last_move(&self) -> GamePiece {
+        self.get_next_move()
+    }
+
+    pub fn valid_move(&self, col: u8) -> bool {
+        col < self.num_columns && self.columns[(col as usize)].have_room()
+    }
+
+    pub fn last_move_location(&self) -> (u8, u8) {
+        (self.last_move, self.columns[(self.last_move as usize)].next_row - 1)
+    }
+
+    pub fn game_piece_at(&self, x: u8, y: u8) -> GamePiece {
+        if x >= self.num_columns || y >= self.num_rows {
+            return GamePiece::Dash;
+        }
+        match self.columns[(x as usize)].rows[(y as usize)] {
+            GamePiece::X => GamePiece::X,
+            GamePiece::O => GamePiece::O,
+            GamePiece::Dash => GamePiece::Dash
+        }
+    }
 }
--- connect_four_engine/src/game/mod.rs
+++ connect_four_engine/src/game/mod.rs
@@ -5,12 +5,27 @@ use rand;
 use rand::Rng;
 
 pub fn play() {
-    let mut board = board::new_board(8, 8);
-    for i in 0..20 {
-        println!("On turn {}", i);
-        board.print();
-        let mymove = rand::thread_rng().gen_range(0, 8);
-        board.play_piece(mymove);
-    }
+    let mut board = board::new_board(8, 8, 4);
     board.print();
+    for i in 0..200 {
+        println!("On turn {}", i);
+        let mut mymove = rand::thread_rng().gen_range(0, 8);
+        while !board.valid_move(mymove){
+            mymove = rand::thread_rng().gen_range(0, 8);
+        }
+        board.play_piece(mymove);
+        board.print();
+        match board.have_winner() {
+            1 => {
+                println!("X won!");
+                break;
+            },
+            2 => {
+                println!("O won!");
+                break;
+            },
+            _ => ()
+        }
+    }
+    
 }
