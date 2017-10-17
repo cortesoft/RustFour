f0d439e325d01f0ebc7b765a863cd3644799c6f8
Daniel Lockhart <cortesoft@gmail.com>
2017-10-17 00:16:25 -0700

    Take that, type system

--- connect_four_engine/src/game/mod.rs
+++ connect_four_engine/src/game/mod.rs
@@ -6,31 +6,48 @@ pub mod players;
 use self::board::*;
 use self::players::Player;
 
-pub fn play<T: Player, U: Player>(mut player1: T, mut player2: U) -> u8 {
-    let mut board = Board::new_board(8, 8, 4);
-    board.print();
-    for i in 0..500 {
-        println!("On turn {}", i);
-        let mymove: u8;
-        if let GamePiece::X = board.get_current_move() {
-            mymove = player1.which_move(&board);
-        } else {
-            mymove = player2.which_move(&board);
-        }
-        println!("Playing {}", mymove);
-        board.play_piece(mymove);
-        board.print();
-        match board.have_winner() {
-            1 => {
-                println!("X won!");
-                return 1;
-            },
-            2 => {
-                println!("O won!");
-                return 2;
-            },
-            _ => ()
+pub struct Game<T: Player, U: Player> {
+    pub player_1: T,
+    pub player_2: U,
+    board_template: Board
+}
+
+impl<T: Player, U: Player> Game<T, U> {
+
+    pub fn new_game(mut player_1: T, mut player_2: U, rows: u8, cols: u8, conn: u8) -> Game<T,U> {
+        let mut board_template = Board::new_board(rows, cols, conn);
+        Game {
+            player_1,
+            player_2,
+            board_template
         }
     }
-    0
+
+    pub fn play_game(&mut self) -> u8 {
+        let mut board = self.board_template.clone();
+        board.print();
+        for i in 0..500 {
+            println!("On turn {}", i);
+            let mymove = if GamePiece::X == board.get_current_move() {
+                self.player_1.which_move(&board)
+            } else {
+                self.player_2.which_move(&board)
+            };
+            println!("Playing {}", mymove);
+            board.play_piece(mymove);
+            board.print();
+            match board.have_winner() {
+                1 => {
+                    println!("X won!");
+                    return 1;
+                },
+                2 => {
+                    println!("O won!");
+                    return 2;
+                },
+                _ => ()
+            }
+        }
+        0
+    }
 }
